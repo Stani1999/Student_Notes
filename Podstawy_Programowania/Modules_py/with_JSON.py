d67a95f1cbcc100f44e5404a2fef92a9afc5fcbd
j.staniszewski99@gmail.com <stani@StudiAsus.>
2025-01-06 02:58:44 +0100

    Add new module with_JSON

---
+++ Podstawy_Programowania/Modules_py/with_JSON.py
@@ -0,0 +1,29 @@
+'''JSON via with open Function Module to load and save data in file'''
+
+import os, json
+
+def ensure_directory_exists(directory: str) -> None:
+    """Create new directory if it doesn't exist yet."""
+    try:
+        os.makedirs(directory, exist_ok=True)
+    except OSError as e:
+        print(f"Błąd podczas tworzenia struktury katalogowej {directory}: {e}")
+        
+def load(file_path: str) -> dict:
+    """Load users data from JSON file."""
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Plik nieznaleziony {file_path}. Tworze pusty plik z na dane.")
+    except json.JSONDecodeError as e:
+        print(f"Błąd podczas dekodowania: {file_path}: {e}")
+    return {"users": []}
+
+def save(users: dict, file_path: str) -> None:
+    """Save file with user data."""
+    ensure_directory_exists(os.path.dirname(file_path))
+    temp_file = file_path + ".tmp"
+    with open(temp_file, "w", encoding="utf-8") as file:
+        json.dump(users, file, indent=4)
+    os.replace(temp_file, file_path)
