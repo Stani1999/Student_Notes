35abb1bf03445654455b5111c4abc125b9ea11db
Jakub Staniszewski <j.staniszewski99@gmail.com>
2026-03-24 10:30:57 +0100

    Add PAD 1-4

---
+++ Semestr_IV/Programowanie_Aplikacji_Desktopowych/KalkulatorIP/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace KalkulatorIP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void B_Oblicz_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IPNetwork2 ipn2 = IPNetwork2.Parse(TBX_InputIP.Text.Trim());
+
+                TB_Result.Text = "";
+                TB_Result.Text += "Adres Sieci: " + ipn2.Network.ToString() + "\n";
+                TB_Result.Text += "Broadcast: " + ipn2.Broadcast.ToString() + "\n";
+                TB_Result.Text += "Maska: " + ipn2.Netmask.ToString() + "\n";
+                TB_Result.Text += "Pierwszy dostępny adres: " + ipn2.FirstUsable.ToString() + "\n";
+                TB_Result.Text += "Ostatni dostępny adres: " + ipn2.LastUsable.ToString() + "\n";
+                TB_Result.Text += "Liczba hostów: " + ipn2.Usable.ToString() + "\n";
+                TB_Result.Text += "CIDR: " + ipn2.Cidr.ToString() + "\n";
+            }
+            catch (Exception ex)
+            {
+                TB_Result.Text = "Błąd: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ Semestr_IV/Programowanie_Aplikacji_Desktopowych/PAD/26-02-25/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _26_02_25
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Monit napewno?
+            if (MessageBox.Show("Czy na pewno?", "Pytanie", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                // Tak
+                MessageBox.Show("No dobra");
+            }
+            else
+            {
+                // Nie
+                MessageBox.Show("aha");
+            }
+        }
+
+        private void bKliknij2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Wyświetlanie daty systemowej
+            DateTime dt = DateTime.Now;
+            bKliknij2.Content = dt.ToString("t");
+        }
+
+        private void bKliknij2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bKliknij2.Content = "YES";
+        }
+
+        private void bKliknij_MouseEnter(object sender, MouseEventArgs e)
+        {
+            bKliknij.Visibility = Visibility.Hidden;
+        }
+
+        private void bKliknij2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
