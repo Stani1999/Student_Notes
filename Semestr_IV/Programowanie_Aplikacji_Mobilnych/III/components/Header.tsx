5cceb166e1c5fd46e4ab5f678f1fca2c484d49a7
Stani1999 <j.staniszewski99@gmail.com>
2026-04-17 13:54:37 +0200

    For Fix

--- Semestr_IV/Programowanie_Aplikacji_Mobilnych/III/components/Header.tsx
+++ Semestr_IV/Programowanie_Aplikacji_Mobilnych/III/components/Header.tsx
@@ -1,26 +1,54 @@
+import { styles } from "@/styles/IndexStyles";
+import { RootStackParamList } from "@/types/Navigation";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
 import { Text, View } from "react-native";
-import { StyleSheet } from "react-native";
+import HomeScreen from "../../screens/HomeScreen";
 
-type HeaderProps = {
-    title: string;
-};
 
-export default function Header({ title }: HeaderProps) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{title}</Text>
-        </View>
-    );
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function DetailsScreen({ route }: any) {
+  const { title, description, location, hour, date, category } = route.params;
+  return (
+    <View style={styles.detailsContainer}>
+      <Text style={styles.detailsTitle}>{title}</Text>
+      <View style={styles.detailsInfoBox}>
+        <Text style={styles.detailsLabel}>Godzina:</Text>
+        <Text style={styles.detailsValue}>{hour}</Text>
+        
+        <Text style={styles.detailsLabel}>Miejsce:</Text>
+        <Text style={styles.detailsValue}>{location}</Text>
+        
+        <Text style={styles.detailsLabel}>Opis:</Text>
+        <Text style={styles.detailsValue}>{description}</Text>
+
+        <Text style={styles.detailsLabel}>Godzina:</Text>
+        <Text style={styles.detailsValue}>{hour}</Text>
+
+        <Text style={styles.detailsLabel}>Data:</Text>
+        <Text style={styles.detailsValue}>{date}</Text>
+
+        <Text style={styles.detailsLabel}>Kategoria:</Text>
+        <Text style={styles.detailsValue}>{category}</Text>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        padding: 20,
-        backgroundColor: "#4a90e2",
-    },
-    title: {
-        color: "white",
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-});
+export default function App() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="Details" 
+        component={DetailsScreen} 
+        options={{ title: 'Szczegóły wydarzenia' }}
+      />
+    </Stack.Navigator>
+  );
+}
